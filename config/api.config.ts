624653488752937c90ab846fbdf3e46878f259f1
MuhireIghor <muhireighor123@gmail.com>
2024-06-13 11:16:00 +0200

    initiated integration

--- config/api.config.ts
+++ config/api.config.ts
@@ -1,17 +1,30 @@
 import axios from "axios"
 const API_URL = 'http://10.5.220.210:4000/api/v1';
-const sendRequest = async (url: string, payload: Object, method: string, headers?: any) => {
+export const sendRequest = async (url: string, payload: Object, method: string, headers?: any) => {
     try {
-        switch (method) {
-            case 'GET':
-                const { data } = await axios.get(`/${API_URL}`,{headers});
-                return data;
-            case 'POST':
-                const {} = await axios.get(`/${API_URL}`,{headers})
+        if (method === 'GET') {
+
+            const resp = await axios.get(`${API_URL}/${url}`, headers);
+            return resp.data;
         }
+        else if (method === 'POST') {
+            const resp = await axios.post(`${API_URL}/${url}`, payload, headers);
+            return resp.data;
+        }
+        else if (method === 'PUT') {
+            const resp = await axios.put(`${API_URL}/${url}`, payload, headers);
+            return resp.data;
+
+        }
+        else if (method === 'DELETE') {
+            const resp = await axios.delete(`${API_URL}/${url}`, headers);
+            return resp.data;
+        }
+
 
     }
     catch (err: any) {
+        console.log(err)
         throw new Error(err.message)
     }
 
